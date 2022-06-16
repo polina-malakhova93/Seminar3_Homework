18709761015ecd8da5b8a029146b2924c6906036
polina993 <>
2022-06-16 21:29:35 +0300

    add task21

---
+++ task21/Program.cs
@@ -0,0 +1,29 @@
+﻿int x1 = Input("Введите координату x1: ");
+int x2 = Input("Введите координату x2: ");
+int y1 = Input("Введите координату y1: ");
+int y2 = Input("Введите координату y2: ");
+int z1 = Input("Введите координату z1: ");
+int z2 = Input("Введите координату z2: ");
+
+int distanceX = CalculateDistance(x1,x2);
+int distanceY = CalculateDistance(y1,y2);
+int distanceZ = CalculateDistance(z1,z2);
+
+Console.WriteLine(Math.Sqrt(Quadro(distanceX) + Quadro(distanceY) + Quadro(distanceZ)));
+
+int Quadro(int a) 
+{
+    return a * a;
+}
+
+int CalculateDistance(int a, int b) 
+{
+    return a-b;
+}
+
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
