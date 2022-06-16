63fdeec8398ae7d78a2f89715a67176bc967d88c
polina993 <>
2022-06-16 21:41:11 +0300

    add task23

---
+++ task23/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while(count < n)
+{
+    int result = Cube(count);
+    count++;
+    Console.Write($"{result}, ");
+}
+Console.WriteLine(Cube (n));
+
+int Cube(int a)
+{
+    return a * a * a;
+}
+
