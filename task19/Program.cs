e04e48b46a97394d5fc9910b0f661c56e46fb24d
polina993 <>
2022-06-16 22:23:55 +0300

    add task19

---
+++ task19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число: "); // 23432
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit = 0;
+int reverse = 0;
+
+while(number > 0)
+{
+    digit = number % 10;
+    reverse = reverse * 10 + digit;
+    number = number / 10;
+}
+
+if(number == reverse)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
